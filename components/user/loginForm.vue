1ed99ef6d92167b61266af5f2b0304caca8f687b
huangfenglin <effort0755@163.com>
2019-11-09 19:04:23 +0800

    登陆页面的布局和登陆功能的校验表单规则

---
+++ components/user/loginForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <!-- model: 指定表单数据对象（不是必须要设置）
+    ref: 获取dom元素（this.$refs.form）
+  rules: 校验规则-->
+  <el-form :model="form" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item" prop="username">
+      <el-input placeholder="用户名/手机" v-model="form.username"></el-input>
+    </el-form-item>
+
+    <el-form-item class="form-item" prop="password">
+      <el-input placeholder="密码" v-model="form.password" type="password"></el-input>
+    </el-form-item>
+
+    <p class="form-text">
+      <nuxt-link to="#">忘记密码</nuxt-link>
+    </p>
+
+    <el-button class="submit" type="primary" @click="handleLoginSubmit">登录</el-button>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 表单数据
+      form: {
+        username: "",
+        password: ""
+      },
+      // 表单规则
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    // 提交登录
+    handleLoginSubmit() {
+      // validate是elementui的form表单的方法，提供一个回调函数校验表单是否通过验证
+      // valid如果值true的话就表示通过
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.$axios({
+            url: "/accounts/login",
+            method: "POST",
+            data: this.form
+          }).then(res => {
+            console.log(res);
+          });
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.form {
+  padding: 25px;
+}
+
+.form-item {
+  margin-bottom: 20px;
+}
+
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
+
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
