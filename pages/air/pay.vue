4c8fd8d66cd2da49ad6cad97cee8a13a29d807ef
huangfenglin <effort0755@163.com>
2019-11-17 20:37:15 +0800

    付款页布局和请求订单详情前端生成二维码

--- pages/air/pay.vue
+++ pages/air/pay.vue
@@ -1,79 +1,110 @@
 <template>
-    <div class="container">
-        <div class="main">
-            <div class="pay-title">
-                支付总金额 <span class="pay-price">￥ 1000</span>
-            </div>
-            <div class="pay-main">
-                <h4>微信支付</h4>
-                <el-row type="flex" 
-                justify="space-between" 
-                align="middle"
-                class="pay-qrcode">
-                    <div class="qrcode">
-                        <!-- 二维码 -->
-                        <canvas id="qrcode-stage"></canvas>
+  <div class="container">
+    <div class="main">
+      <div class="pay-title">
+        支付总金额
+        <span class="pay-price">￥ {{order.price | tofixed}}</span>
+      </div>
+      <div class="pay-main">
+        <h4>微信支付</h4>
+        <el-row type="flex" justify="space-between" align="middle" class="pay-qrcode">
+          <div class="qrcode">
+            <!-- 二维码 -->
+            <canvas id="qrcode-stage"></canvas>
 
-                        <p>请使用微信扫一扫</p>
-                        <p>扫描二维码支付</p>
-                    </div>
-                    <div class="pay-example">
-                        <img src="http://157.122.54.189:9093/images/wx-sweep2.jpg" alt="">
-                    </div>
-                </el-row>
-            </div>
-        </div>
+            <p>请使用微信扫一扫</p>
+            <p>扫描二维码支付</p>
+          </div>
+          <div class="pay-example">
+            <img src="http://157.122.54.189:9093/images/wx-sweep2.jpg" alt />
+          </div>
+        </el-row>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import QRCode from "qrcode";
 export default {
-    
-}
+  data() {
+    return {
+      order: {}
+    };
+  },
+  mounted(){
+    // 获取订单id
+    const { id } = this.$route.query;
+    // 定时器主要为了等待本地存储把值返回给store，才可以获取到token
+    setTimeout(() => {
+      // 请求订单详情
+      this.$axios({
+        url: "/airorders/" + id,
+        headers: {
+          // Bearer属于jwt的token标准
+          Authorization: "Bearer " + this.$store.state.user.userInfo.token
+        }
+      }).then(res => {
+        this.order = res.data;
+        // 生成二维码
+        const canvas = document.querySelector("#qrcode-stage")
+         // 第一个参数是canvas dom元素, 要生成二位码的链接
+         QRCode.toCanvas(canvas,this.order.payInfo.code_url,{
+             width: 200
+         });
+      })
+    }, 1)
+  },
+  filters: {
+    tofixed(value) {
+      return Number(value || 0).toFixed(2);
+    }
+  }
+};
 </script>
 
 <style scoped lang="less">
-.container{
-    background:#f5f5f5;
-    padding: 30px 0;
-    .main{
-        width:1000px;
-        margin:0 auto;
-        .pay-title{
-            text-align: right;
-            span{
-                font-size:28px;
-                color:orangered;
-            }
-        }
-        .pay-main{
-            background:#fff;
-            margin-top:10px;
-            border-top: 5px orange solid;
-            padding:30px;
-            h4{
-                font-size: 28px;
-                font-weight: normal;
-                margin-bottom: 10px;
-            }
-            .pay-qrcode{
-                padding:0 80px;
-            }
-            .qrcode{
-                border:1px #ddd solid;
-                padding:15px;
-                height: fit-content;
-                #qrcode-stage{
-                    width:200px;
-                    height:200px;
-                    margin-bottom: 10px;
-                }
-                p{
-                    line-height: 2;
-                    text-align: center;
-                }
-            }
-        }
+.container {
+  background: #f5f5f5;
+  padding: 30px 0;
+  .main {
+    width: 1000px;
+    margin: 0 auto;
+    .pay-title {
+      text-align: right;
+      span {
+        font-size: 28px;
+        color: orangered;
+      }
     }
+    .pay-main {
+      background: #fff;
+      margin-top: 10px;
+      border-top: 5px orange solid;
+      padding: 30px;
+      h4 {
+        font-size: 28px;
+        font-weight: normal;
+        margin-bottom: 10px;
+      }
+      .pay-qrcode {
+        padding: 0 80px;
+      }
+      .qrcode {
+        border: 1px #ddd solid;
+        padding: 15px;
+        height: fit-content;
+        #qrcode-stage {
+          width: 200px;
+          height: 200px;
+          margin-bottom: 10px;
+        }
+        p {
+          line-height: 2;
+          text-align: center;
+        }
+      }
+    }
+  }
 }
 </style> 
